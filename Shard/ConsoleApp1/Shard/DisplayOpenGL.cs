43db66d90405842df74f43490f58cbcb33122f61
Pascal Walloner <st170276@stud.uni-stuttgart.de>
2023-02-01 14:09:19 +0100

    Projektdateien hinzufügen.

---
+++ Shard/ConsoleApp1/Shard/DisplayOpenGL.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Windowing.Desktop;
+
+namespace Shard
+{
+    class DisplayOpenGL : Display
+    {
+        private WindowGL _window;
+
+
+        public override void clearDisplay()
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            _window.ProcessWindowEvents();
+        }
+
+        public override void display()
+        {
+            _window.Display();
+        }
+
+        public override void initialize()
+        {
+
+            _window = new WindowGL();
+            _window.Initialize();
+        }
+
+        public override void showText(string text, double x, double y, int size, int r, int g, int b)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public override void showText(char[,] text, double x, double y, int size, int r, int g, int b)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ Shard/ConsoleApp1/Shard/WindowGL.cs
@@ -0,0 +1,35 @@
+﻿using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shard
+{
+    class WindowGL : GameWindow
+    {
+        public WindowGL() : base(GameWindowSettings.Default, NativeWindowSettings.Default)
+        {
+        }
+
+        public void Initialize()
+        {
+            base.Context?.MakeCurrent();
+            OnLoad();
+            OnResize(new ResizeEventArgs(base.Size));
+        }
+
+        public void Display()
+        {
+            SwapBuffers();
+        }
+
+        public void ProcessWindowEvents()
+        {
+            NativeWindow.ProcessWindowEvents(base.IsEventDriven);
+        }
+
+    }
+}
