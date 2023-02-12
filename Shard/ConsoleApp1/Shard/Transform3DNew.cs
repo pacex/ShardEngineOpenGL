1d1baa1312b89d3ebcc0b49ac94259190519934c
Pascal Walloner <st170276@stud.uni-stuttgart.de>
2023-02-12 13:33:40 +0100

    Add new transform class

---
+++ Shard/ConsoleApp1/Shard/Transform3DNew.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Mathematics;
+
+namespace Shard
+{
+    class Transform3DNew
+    {
+        public Vector3 Translation;
+        public Quaternion Rotation;
+        public Vector3 Scale;
+    }
+}
